25885f3342151cae7c71d8b64206ebc9e8bbf8b1
Myaon <massiro.myaon@gmail.com>
2022-12-10 18:17:53 +0900

    マイク音量検出
    https://note.com/hikohiro/n/n2b756a0dd2b7

--- Assets/Scripts/CompassSensor.cs
+++ Assets/Scripts/CompassSensor.cs
@@ -13,12 +13,13 @@ public class CompassSensor : MonoBehaviour {
         Input.compass.enabled = true;
         Input.location.Start ();
 
-        Debug.Log (string.Format("<b>精度</b>：{0}", Input.compass.headingAccuracy));
-        Debug.Log (string.Format("<b>タイムスタンプ</b>：{0}", Input.compass.timestamp));
+        //Debug.Log (string.Format("<b>精度</b>：{0}", Input.compass.headingAccuracy));
+        //Debug.Log (string.Format("<b>タイムスタンプ</b>：{0}", Input.compass.timestamp));
 
     }
     
     void Update () {
+        /*
         Debug.Log (
             string.Format ("magneticHeading : {0}, rawVector : {1}, trueHeading : {2}",
                 Input.compass.magneticHeading,
@@ -26,6 +27,7 @@ public class CompassSensor : MonoBehaviour {
                 Input.compass.trueHeading
             )
         );
+        */
 
         compass_message.text = string.Format ("コンパス : {0}",
             Input.compass.trueHeading
